b59fc306cfc2201546c877d06e3a671ec35051e9
macbookpro <dominik.wojtys@gmail.com>
2021-07-08 19:42:17 +0200

    Initial Commit

---
+++ SwiftUIMilestone 7-9/SwiftUIMilestone_7_9App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMilestone_7_9App.swift
+//  SwiftUIMilestone 7-9
+//
+//  Created by MacBook Pro on 08/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMilestone_7_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
