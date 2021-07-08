3faa408ee1aaaf97ad525cecee0ffebbad4ed55a
macbookpro <dominik.wojtys@gmail.com>
2021-07-08 22:58:26 +0200

    Add saving to user defaults

--- SwiftUIMilestone 7-9/Activity.swift
+++ SwiftUIMilestone 7-9/Activity.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct ActivityItem: Identifiable {
+struct ActivityItem: Identifiable, Codable {
     var title: String
     var description: String
     var counter: Int = 0
@@ -20,9 +20,24 @@ struct ActivityItem: Identifiable {
 }
 
 class Acitivities: ObservableObject {
-    @Published var items = [ActivityItem]()
+    @Published var items = [ActivityItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Activities")
+            }
+        }
+    }
     
     init() {
+        if let items = UserDefaults.standard.data(forKey: "Activities") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ActivityItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        
         self.items = []
     }
 }
